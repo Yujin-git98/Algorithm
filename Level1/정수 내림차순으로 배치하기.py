6907d07a4e9651fd191b891e689c5d99125416f3
Yujin-git98 <83621692+Yujin-git98@users.noreply.github.com>
2022-10-01 15:10:01 +0900

    Update 정수 내림차순으로 배치하기.py

--- Level1/정수 내림차순으로 배치하기.py
+++ Level1/정수 내림차순으로 배치하기.py
@@ -1,1 +1,13 @@
 
+# 내 코드
+def solution(n):
+    answer=list(int(i) for i in str(n))
+    answer.sort(reverse=True)
+    answer =''.join(str(s) for s in answer)
+    return int(answer)
+
+# 참고 코드 1. for문 필요없이 str로 바꿔줌
+def solution(n):
+    ls = list(str(n))
+    ls.sort(reverse = True)
+    return int("".join(ls))
